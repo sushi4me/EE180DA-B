ddad2bcfd195e2bed5bb2d5d7dacb60ff87901ca
Fernando Pacheco <fpacheco0707@yahoo.com>
2016-12-06 12:28:52 -0800

    Adds documentation and refactoring.

--- networks.py
+++ networks.py
@@ -1,8 +1,24 @@
-#!/usr/bin/env python
+#!/usr/bin/python
+
+# Takes two files containing MAC addresses and
+# signal values formatted in columns. Script
+# will output addresses contained in both files.
 
 import sys
 from optparse import OptionParser
 
+def getPairs(filename):
+    MAC_ADDR = {}
+    with open(filename) as f:
+        for line in f:
+            key, val = line.split()
+            MAC_ADDR[key] = val
+    
+    return MAC_ADDR
+
+def compareAddresses(dict1, dict2):
+    return {x:dict1[x] for x in dict1 if x in dict2}
+ 
 def main():
     version_msg = "%prog 1.0"
     usage_msg = """%prog [OPTION]... MACADDR_FILE1 MACADDR_FILE2
@@ -19,21 +35,19 @@ def main():
 
     deviceFile1 = args[0]
     deviceFile2 = args[1]
+    
+    MAC_ADDR1 = getPairs(deviceFile1)
+    MAC_ADDR2 = getPairs(deviceFile2)
 
-    with open(deviceFile1) as f:
-        MAC_ADDR1 = f.readlines()
-
-    with open(deviceFile2) as f:
-        MAC_ADDR2 = f.readlines()
-
-    commonAddr = [line for line in MAC_ADDR1 if line in MAC_ADDR2]
+    commonAddr = compareAddresses(MAC_ADDR1, MAC_ADDR2)
 
     if options.file is not None:
-        with open(options.file) as f:
-            f.writelines(commonAddr)
+        with open(options.file, "a") as f:
+            for key,val in commonAddr.items():
+                f.write(key + " " + val + "\n")
     else:
-        for line in commonAddr:
-            sys.stdout.write(line)
+        for key,val in commonAddr.items():
+            print key, val
 
 if __name__=="__main__":
     main()
