58eaa4ae04ccb0b1bf8d979347778375a1a39144
Fernando Pacheco <fpacheco0707@yahoo.com>
2016-12-06 13:35:29 -0800

    Modifies output format.

--- detect.sh
+++ detect.sh
@@ -4,5 +4,5 @@ LOCATION=$1 # Text file to save output.
 
 
 iwlist wlan0 scan | grep 'Address\|Signal' | \
-	sed 's/^.*Address: //; s/^.*level=-\([0-9]\{2\}\) dBm/\1/' > \
-	$LOCATION
+	sed 's/^.*Address: //; s/^.*\([0-9]\{2\}\) dBm/\1/' | \
+	sed 'N; s/\n/ /' > $LOCATION
