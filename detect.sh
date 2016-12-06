85c2c552ad6757d1aa11be16843330fef30d4505
Fernando Pacheco <fpacheco0707@yahoo.com>
2016-12-05 21:59:51 -0800

    Adds script argument.

--- detect.sh
+++ detect.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-iwlist wlan0 scan | grep 'Address\|Signal' | sed 's/^.*Address: //; s/^.*level=-\([0-9]\{2\}\) dBm/\1/' > E1.txt
+LOCATION=$1 # Text file to save output.	
+
+
+iwlist wlan0 scan | grep 'Address\|Signal' | \
+	sed 's/^.*Address: //; s/^.*level=-\([0-9]\{2\}\) dBm/\1/' > \
+	$LOCATION
