6e0dacf995e3967264877fd9c88d0bbe2a5e0e5a
Fernando Pacheco <fpacheco0707@yahoo.com>
2016-12-05 21:48:27 -0800

    Removes unnecessary while loop.

--- detect.sh
+++ detect.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-count=0
-while [ $count -lt 1 ]; do
-	iwlist wlan0 scan | grep 'Address\|Signal' | sed 's/^.*Address: //; s/^.*level=-\([0-9]\{2\}\) dBm/\1/' > E1.txt
-	let count=count+1
-	#sleep 15
-done
+iwlist wlan0 scan | grep 'Address\|Signal' | sed 's/^.*Address: //; s/^.*level=-\([0-9]\{2\}\) dBm/\1/' > E1.txt
