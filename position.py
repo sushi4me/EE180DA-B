fc316ab97d5aec47047c7cad2680808f9ba6cb6e
Fernando Pacheco <fpacheco0707@yahoo.com>
2016-12-10 11:53:13 -0800

    Changes position estimation algorithm

--- position.py
+++ position.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import sys
+from os import listdir
+from os.path import isfile, join
 from optparse import OptionParser
 from utilities import getPairs
 
@@ -20,37 +22,39 @@ def main():
         parser.error("Wrong number of operands.")
 
     inputFile = args[0]
-    networksFile = "locations_data/networks.txt"
-    locData1 = "locations_data/location1.txt"
-    locData2 = "locations_data/location2.txt"
 
-    rawInput = getPairs(inputFile)
-    loc1 = getPairs(locData1)
-    loc2 = getPairs(locData2)
-    with open(networksFile) as f:
-        networksList = f.readlines()
-
-    dist1 = []
-    dist2 = []
+    # Collect files from reference db.
+    locationsDir = "locations_data"
+    locationFiles = [join(locationsDir, f) for f in listdir(locationsDir) if isfile(join(locationsDir, f))]
     
-    for line in networksList:
-        if line in rawInput:
-            dist1.append(pow(rawInput[line] - loc1[line], 2))
-            dist2.append(pow(rawInput[line] - loc2[line], 2))
-        else:
-            dist1.append(0)
-            dist2.append(0)
-    
-    score1 = sum(dist1)
-    score2 = sum(dist2)
+    # Parse RSSI, place in dictionary with MAC address as key.
+    rssiObserved = getPairs(inputFile)
+    rssiReferences = [getPairs(file) for file in locationFiles]
 
-    msg = ""
-    if score1 < score2:
-        msg = "You are probably at position 1"
+    distances = [[] for i in range(len(rssiReferences))] # Euclidean distances.
+
+    # For each file in reference db, compute euclidean distance
+    # between observed RSSI and reference RSSI.
+    for i, rssiReference in enumerate(rssiReferences):
+        for addr, rssi in rssiReference.items():
+            if addr in rssiObserved:
+                dist = pow(rssiObserved[addr] - rssi, 2)
+            else: # MAC Address not found, add arbitrarily large value.
+                dist = 100 # TO-DO: Replace value.
+
+            distances[i].append(dist)
+
+    scores = [sum(d) for d in distances]
+
+    pos = scores.index(min(scores))
+
+    # TO-DO: Print coordinates of location instead of location filename.
+    msg = "You are probably at " + locationFiles[pos]
+    if options.file is not None:
+        with open(options.file, "w") as f:
+            f.write(msg + "\n")
     else:
-        msg = "You are probably at position 2"
-
-    print msg
+        print msg
 
 if __name__ == "__main__":
     main()
