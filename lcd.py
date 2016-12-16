5a83d965278129b2782f0d57fd40a9cb2acb01a5
Edison <edavidvallejo@gmail.com>
2016-12-16 00:13:42 +0000

    Created gameStatus, capturedFlag, droppedFlag, and gotPowerUp modules for lcd class

--- lcd.py
+++ lcd.py
@@ -6,7 +6,6 @@
 # http://www.instructables.com/id/Show-the-Intel-Edison-WiFi-IP-Address-on-a-Grove-L/step2/Run-the-Code/
 #
 # Permission is hereby granted, free of charge, to any person obtaining
-#
 # a copy of this software and associated documentation files (the
 # "Software"), to deal in the Software without restriction, including
 # without limitation the rights to use, copy, modify, merge, publish,
@@ -28,17 +27,18 @@
 #----------------------------
 # Modules
 #----------------------------
-import fcntl
-import struct
 import pyupm_i2clcd as lcd
-
+import time
 #----------------------------
 # Globals
 #----------------------------
 R="red"
 G="green"
 B="blue"
-
+P="pink"
+Y="yellow"
+T="teal"
+OFF="off"
 #----------------------------
 # LCD Class
 #----------------------------
@@ -49,26 +49,73 @@ class lcd:
 
 	# Clear
 	myLcd.clear()
-        
+       
         # Takes 4 Parameters: string, posX, posY, color=DefaultWhite
 	def display(self, string, posX, posY, color=None):
-                self.myLcd.clear()
+		# Clear
+		self.myLcd.clear()
                 # Green
-		if color == 'green':
-			self.myLcd.setColor(255, 255, 0)
+		if color == G:
+			self.myLcd.setColor(0, 255, 0)
 		# Red
-		elif color == 'red':
-			self.myLcd.setColor(255, 0, 0)
+		elif color == R:
+			self.myLcd.setColor(150, 0, 0)
 		# Blue
-		elif color == 'blue':
-			self.myLcd.setColor(0, 0, 255)
-		# Default: White
+		elif color == B:
+			self.myLcd.setColor(25, 25, 255)
+                # Pink
+		elif color == P:
+			self.myLcd.setColor(255, 0, 255)
+		# Teal
+		elif color == T:
+			self.myLcd.setColor(0, 255, 255)
+		# Yellow
+		elif color == Y:
+			self.myLcd.setColor(255, 255, 0)
+                # OFF
+                elif color == OFF:
+                        self.myLcd.setColor(0, 0, 0)
+                # Default: White
 		else:
 			self.myLcd.setColor(255, 255, 255)
-		
                 # Set the cursor
 		self.myLcd.setCursor(posX,posY)
 		# Print it.
 		self.myLcd.write(string)
+                # OFF
+                if color == OFF:
+                    self.myLcd.clear()
 
+        def gameStatus(self, string):
+            self.display(string, 0, 0, Y)
+        
+        def capturedFlag(self):
+            self.display("CAPTURED FLAG!", 0, 0, G)
+        
+        def droppedFlag(self):
+            self.display("DROPPED FLAG!", 0, 0, R)
 
+        def frozen(self):
+            self.display("FROZEN! burr!", 0, 0, B)
+
+        def gotPowerUp(self, string):
+            message = string + " POWERUP!"
+            sleeptime = 0.2
+            posY=0
+            for posY in range(0, 1):
+                posX = 0
+                for posX in range(0,7):
+                    self.display(message, 0, 0, R)
+                    time.sleep(sleeptime)
+                    self.display(message, 0, 0, G)
+                    time.sleep(sleeptime)
+                    self.display(message, 0, 0, B)
+                    time.sleep(sleeptime)
+                    self.display(message, 0, 0, P)
+                    time.sleep(sleeptime)
+                    self.display(message, 0, 0, Y)
+                    time.sleep(sleeptime)
+                    self.display(message, 0, 0, T)
+                    time.sleep(sleeptime)
+                    posX += 1
+            posY +=2
