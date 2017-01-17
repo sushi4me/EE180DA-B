83b02ec5ed094aba5c3f27842709ec8839180aa5
Fernando Pacheco <fpacheco0707@yahoo.com>
2017-01-16 21:02:55 -0800

    Changes file naming convention.

--- detect.sh
+++ detect.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-LOCATION=$1 # Text file to save output.	
+if [ $# -eq 0 ]
+then
+	echo -e "Error: Wrong number of operands\nUsage: $0 FILE_PREFIX"
+	exit 1
+fi
 
+POSITION=$1
+TSTAMP=$(date +%s)
+FILENAME="${POSITION}_${TSTAMP}.txt"
 
-iwlist wlan0 scan | grep 'Address\|Signal' | \
-	sed 's/^.*Address: //; s/^.*\([0-9]\{2\}\) dBm/\1/' | \
-	sed 'N; s/\n/ /' > $LOCATION
+iwlist wlan0 scan | grep 'Address:\|Signal' | sed 's/.*Address: //; s/.*\([0-9]\{2\}\) dBm/\1/' | sed 'N; s/\n/ /' > $FILENAME
