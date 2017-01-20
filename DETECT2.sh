323a3bb5a725637dd6fa4d30d49b9a18df14c1ca
sushi4me <nathankong1688@yahoo.com>
2017-01-19 18:13:13 -0800

    New location file thing USE ME

---
+++ DETECT2.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+printf "Choose a spot <1, 17, 25, 35, 41, 47> : "
+read POSITION
+
+while [ "$POSITION" -ne 1 ] && [ "$POSITION" -ne 17 ] && [ "$POSITION" -ne 25 ] && [ "$POSITION" -ne 35 ] && [ "$POSITION" -ne 41 ] && [ "$POSITION" -ne 47 ]; do
+	printf "Wrong starting location, try again: "
+	unset POSITION
+	read POSITION
+done
+
+while [ 0 ]; do
+
+	TSTAMP=$(date +%s)
+	FILENAME="${POSITION}_${TSTAMP}.txt"
+
+	printf "SCANNING... PLEASE WAIT!\n"
+	iwlist wlan0 scan | grep 'Address:\|Signal' | sed 's/.*Address: //; s/.*\([0-9]\{2\}\) dBm/\1/' | sed 'N; s/\n/ /' > $FILENAME
+	printf "DONE SCANNING!\n"	
+
+	printf "Go to next position and enter next command <q, f, r> : "
+	read VAR
+
+	case $VAR in
+	"q")
+		printf "Quitting!\n"
+		exit
+		;;
+	"f")
+		((POSITION++))
+		;;
+	"r")
+		printf "Repeating...\n"
+		;;
+	*)
+		printf "Invalid option, repeating...\n"		
+		;;
+	esac
+
+done
