a821667dc659ede43c25744d51bfe43dd08e9a3b
sushi4me <nathankong1688@yahoo.com>
2017-01-21 13:14:23 -0800

    Edited detect.sh to reset after reaching position 60.  Also uploading test data.

--- detect.sh
+++ detect.sh
@@ -7,7 +7,7 @@ fi
 printf "Choose a spot <0, 17, 25, 35, 41, 47> : "
 read POSITION
 
-while [ "$POSITION" -ne 0 ] && [ "$POSITION" -ne 17 ] && [ "$POSITION" -ne 25 ] && [ "$POSITION" -ne 35 ] && [ "$POSITION" -ne 41 ] && [ "$POSITION" -ne 47 ]; do
+while [ "$POSITION" -ne 59 ] && [ "$POSITION" -ne 17 ] && [ "$POSITION" -ne 25 ] && [ "$POSITION" -ne 35 ] && [ "$POSITION" -ne 41 ] && [ "$POSITION" -ne 47 ]; do
 	printf "Wrong starting location, try again: "
 	unset POSITION
 	read POSITION
@@ -24,6 +24,11 @@ while [ 0 ]; do
 	iwlist wlan0 scan | grep 'Address:\|Signal' | sed 's/.*Address: //; s/.*\([0-9]\{2\}\) dBm/\1/' | sed 'N; s/\n/ /' > $FILENAME
 	printf "DONE SCANNING!\n"	
 
+	if [ $POSITION == 60 ]
+	then
+		printf "Reached last position.  Selecting (n) will take you to position 0\n"	
+	fi
+
 	printf "Go to next position (n) or rescan current position (r).  Use (q) to quit <n, r, q> : "
 	read VAR
 
@@ -36,8 +41,9 @@ while [ 0 ]; do
 		if [ $POSITION == 60 ]
 		then
 			POSITION=0
+		else
+			((POSITION++))
 		fi
-		((POSITION++))
 		;;
 	"r")
 		printf "Repeating...\n"
