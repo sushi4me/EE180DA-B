0349a10837af8011b27581671ff1740f797f32c3
Edison Vallejo <edavidvallejo@gmail.com>
2017-01-22 21:19:52 -0800

    added mini map to indicate starting position

--- detect.sh
+++ detect.sh
@@ -4,7 +4,14 @@ if [ ! -d "locations_data" ]; then
 	mkdir locations_data
 fi
 
-printf "Choose a spot <0, 17, 25, 35, 41, 47> : "
+printf "(0)  Hallway 1              41----47
+(17) Hallway 2               |    |
+(25) Hallway 3      25------35    |
+(35) Hallway 4       |            |
+(41) Hallway 5       |            |
+(47) Hallway 6      17------------0 (EIV 44110)
+===============================================
+Choose a starting position < 0, 17, 25, 35, 41, 47 > : "
 read POSITION
 
 while [ "$POSITION" -ne 0 ] && [ "$POSITION" -ne 17 ] && [ "$POSITION" -ne 25 ] && [ "$POSITION" -ne 35 ] && [ "$POSITION" -ne 41 ] && [ "$POSITION" -ne 47 ]; do
