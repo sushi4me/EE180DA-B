74644ba22e775eae29297ec67d1676d3794abb03
sushi4me <nathankong1688@yahoo.com>
2017-02-01 19:22:48 -0800

    To IE

--- json_client.py
+++ json_client.py
@@ -22,6 +22,7 @@ from subprocess import call
 import sys
 import time
 
+"""
 BUTTON_UP = 	mraa.Gpio(47, owner=False, raw=True)
 BUTTON_DOWN = 	mraa.Gpio(44, owner=False, raw=True)
 BUTTON_LEFT = 	mraa.Gpio(165, owner=False, raw=True)
@@ -37,11 +38,12 @@ BUTTON_RIGHT.dir(mraa.DIR_IN)
 BUTTON_SELECT.dir(mraa.DIR_IN)
 BUTTON_A.dir(mraa.DIR_IN)
 BUTTON_B.dir(mraa.DIR_IN)
+"""
 
 def dof_function():
 	m_OLED = OLED()
 	while True:
-		if BUTTON_A.read() == 0:
+		if m_OLED.BUTTON_A.read() == 0:
 			m_OLED.write("Hello")
 
 def network_connect(host_name, post):
