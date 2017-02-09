1f0953bbc46a9ccfe46c65b86115ed57df5cbd12
sushi4me <nathankong1688@yahoo.com>
2017-02-08 18:02:31 -0800

    Changed from forking/threads to Processes

--- json_server.py
+++ json_server.py
@@ -2,13 +2,15 @@
 
 import json
 from modules.jsonsocket import Server
+from multiprocessing import Process, Pipe
 from optparse import OptionParser
 import os
 import socket
 from subprocess import call
 import sys
 
-def new_socket_connection(server):
+def client_connect(server, num):
+	server.send({'player_num' : num})
 	while True:	
 		print server.recv()
 	'''
@@ -18,8 +20,9 @@ def new_socket_connection(server):
 		finally:
 			c.close()
 	'''
+
 def main():
-	#Define option parse messages/options
+	# Define option parse messages/options
 	version_msg = "server_12.22.16"
 	usage_msg = """%prog [OPTIONS] ...
 	Hosts server on HOST & waits for clients to connect."""
@@ -31,7 +34,7 @@ def main():
 	options, args = parser.parse_args(sys.argv[1:])
 	
 	"""	
-	#Create a TCP/IP socket
+	# Create a TCP/IP socket
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 	if options.specific_host is not None:
@@ -39,7 +42,7 @@ def main():
 	else:
 		server_name = "Nathan-Laptop.hawaii.rr.com" #default host
 
-	#Connect
+	# Connect
 	server_address = (server_name, 8000)
 	s.bind(server_address)
 
@@ -55,19 +58,24 @@ def main():
 		if input() == 'q': break
 	"""
 
+	# Set up server variables
 	#host = 'wifi-131-179-2-179.host.ucla.edu'
 	host = 'localhost'	
 	port = 8888
 	server = Server(host, port)
+	client_process_list = []
+	count = 0
 
+	# Accept incoming connections and make a thread for it
 	while True:
 		server.accept()
-		newpid = os.fork()
-		if newpid == 0:
-			while True:
-				data = server.recv()
-				print data
+		process = Process(target=client_connect, name=count, args=(server, count ))
+		count += 1
+		client_process_list.append(process)
+		process.start
 
+	# Join & close server
 	server.close()
+
 if __name__ == "__main__":
 	main()
