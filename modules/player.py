8fd6a6b4852b91b22556a5a687bb12fce46e1020
sushi4me <nathankong1688@yahoo.com>
2017-02-11 19:43:05 -0800

    Changes to player object

--- modules/player.py
+++ modules/player.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python
-#******************************************************************************
-# Module: Player.py
-#
-# Description:
-#
-#
-#
-# Version: 1.01 created 
-# 
-# Revisions:
-#******************************************************************************
 
-#----------------------------
-# Modules
-#----------------------------
-
-#----------------------------
-# Globals
-#----------------------------
-
-#----------------------------
-# Player Class
-#----------------------------
 class Player:
-    team = 0
-    def __init__(self, team):
-        self.team = team
-        
+	m_client = None;
+	m_location = None;
+	m_player_num = None;
+	m_status = 0;
+
+	def setClient(self, client):
+		m_client = client
+
+	def setLocation(self, location):
+		m_location = location
+
+	def setPlayerNum(self, player_num):
+		m_player_num = player_num
+
+	def setStatus(self, status):
+		m_status = status
+
+	def getClient(self):
+		return self.m_client
+
+	def getLocation(self):
+		return self.m_location
+
+	def getPlayerNum(self):
+		return self.m_player_num
+
+	def getStatus(self):        
+		return self.status
