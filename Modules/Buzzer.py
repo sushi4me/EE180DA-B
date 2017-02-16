08a4709968d2f49e1aa143cf6f0a14f38e9c70b7
Edison <edavidvallejo@gmail.com>
2017-02-16 05:10:17 -0800

    implemented connected, disconnected, shoot, cloak, hit, and powerUp methods

--- Modules/Buzzer.py
+++ Modules/Buzzer.py
@@ -1,25 +1,102 @@
+#------------------------
+# Imported Modules
+#------------------------
 import time
 import pyupm_buzzer as upmBuzzer
 import mraa as m
 
-# Create the buzzer object using GPIO pin 5
+#------------------------
+#  Buzzer Class
+#------------------------
+class Buzzer:
+    #------------------------
+    # Member Variables
+    #------------------------
+    # Time tick = 1/32 s;  1000000 = 1s
+    tick = 31250 
+    # Time between notes
+    sleeptime = 0.01
 
-buzzer = upmBuzzer.Buzzer(47)
-chords = [upmBuzzer.DO, upmBuzzer.RE, upmBuzzer.MI, upmBuzzer.FA,
-          upmBuzzer.SOL, upmBuzzer.LA, upmBuzzer.SI, upmBuzzer.DO,
-          upmBuzzer.SI];
+    #------------------------
+    #  Connected Sound
+    #------------------------
+    sound_connected = [ 2, 1, 0, 4 ]
+    beat_connected = [ 4, 2, 2, 4 ]
+    
+    #------------------------
+    #  Disconnected Sound
+    #------------------------
+    sound_disconnected = [ 5, 6, 7, 2, 1, 0 ]
+    beat_disconnected = [ 3, 4, 5, 6, 7, 7 ]
+   
+    #------------------------
+    #  Shoot
+    #------------------------
+    sound_shoot = [ 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
+    beat_shoot = [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
+    
+    #------------------------
+    #  Hit
+    #------------------------
+    sound_hit = [ 8, 0 ]
+    beat_hit = [ 12, 8 ]
+   
+    #------------------------
+    # Cloak
+    #------------------------
+    sound_cloak = [ 0, 8, 0, 7, 0, 6, 0, 5, 0, 4, 0, 3, 0, 2, 0, 1, 0, 0 ]
+    beat_cloak = [ 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5 ]
 
-# Print sensor name
-print buzzer.name()
+    #------------------------
+    # Power Up
+    #------------------------
+    sound_powerUp = [ 1, 1, 1, 4, 8 ] 
+    beat_powerup = [ 4, 4, 4, 32, 16 ] 
 
-# Play sound (DO, RE, MI, etc.), pausing for 0.1 seconds between notes
-for chord_ind in range (0,7):
-    # play each note for one second
-    print buzzer.playSound(chords[chord_ind], 1000000)
-    time.sleep(0.1)
+    
+    def __init__(self):
+        self.buzzer = upmBuzzer.Buzzer(0)
+        self.chords = [upmBuzzer.DO, upmBuzzer.RE, upmBuzzer.MI, upmBuzzer.FA,
+                        upmBuzzer.SOL, upmBuzzer.LA, upmBuzzer.SI, upmBuzzer.DO,
+                        upmBuzzer.SI, 4000, 6000];
+        self.buzzer.stopSound()
+        self.buzzer.setVolume(0.01)
 
-print "exiting application"
+    # Print sensor name
+    def getName(self):
+        print self.buzzer.name()
 
-# Delete the buzzer object
-del buzzer
+    # Play sound
+    def play(self, sound, beat):
+        for i in range (0, len(sound)):
+            print self.buzzer.playSound(self.chords[sound[i]], 
+                    beat[i] * self.tick)
+            time.sleep(self.sleeptime)
+
+    # Play Connected Sound
+    def connected(self):
+        self.play(self.sound_connected, self.beat_connected)
+
+    # Play Disconnected Sound
+    def disconnected(self):
+        self.play(self.sound_disconnected, self.beat_disconnected)
+    
+    # Play Shoot
+    def shoot(self):
+        self.play(self.sound_shoot, self.beat_shoot)
+
+    # Play Hit
+    def hit(self):
+        self.play(self.sound_hit, self.beat_hit)
+    
+    # Play Cloak
+    def cloak(self):
+        self.play(self.sound_cloak, self.beat_cloak)
+
+    # Play Power up
+    def powerUp(self):
+        self.play(self.sound_powerUp, self.beat_powerup)
+
+    def __del__(self):
+        del self.buzzer
 
