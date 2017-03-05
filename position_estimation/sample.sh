8b7b6ec8c84518256100cdfa7dbc9eec516c9991
Fernando Pacheco <fpacheco0707@yahoo.com>
2017-03-05 23:23:29 +0000

    Changes sample script to collect variable number of samples

--- position_estimation/sample.sh
+++ position_estimation/sample.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]; then
+	echo "Please provide a number of samples."
+	exit 1
+fi
+
+NUMSAMPLES=$1
+
 COUNTER=0
 
 DIR="/home/root/EE180DA-B/position_estimation/observed_rssi/"
 
-while [ $COUNTER -lt 3 ]; do 
+while [ $COUNTER -lt $NUMSAMPLES ]; do 
 	FILENAME="${DIR}sample${COUNTER}.txt"
 	iwlist wlan0 scan | grep 'Address:\|Signal' | sed 's/.*Address: //; s/.*\([0-9]\{2\}\) dBm/\1/' | sed 'N; s/\n/ /' > $FILENAME
 	let COUNTER=COUNTER+1
