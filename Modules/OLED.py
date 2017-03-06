7edf3f8b8253e943a7a30ab3ea0bf7a035958c24
Edison <edavidvallejo@gmail.com>
2017-03-06 15:30:48 -0800

    Added modules to draw MAP with player location

--- Modules/OLED.py
+++ Modules/OLED.py
@@ -1,7 +1,15 @@
+#------------------------------------------------
+# Module: OLED.py
+# Description:
+#
+#------------------------------------------------
 import time
 import mraa
 import pyupm_i2clcd as oledObj
 
+#--------------------------------------
+# OLED Class (Sparkfun OLED Block)
+#--------------------------------------
 class OLED:
 	# Instantiate new OLED object
 	oled = oledObj.EBOLED()
@@ -19,9 +27,10 @@ class OLED:
 	TYPE_DELAY = 0.005		# Delay for writeScroll()
 	MAX_PIXELS_COL = 64
 	MAX_PIXELS_ROW = 48
+	
 
 	#----------------------------------
-	# GPIO OLED Buttons
+	# GPIO OLED Block Buttons
 	#----------------------------------
 	BUTTON_UP = 	mraa.Gpio(46)
 	BUTTON_DOWN = 	mraa.Gpio(31)
@@ -226,55 +235,128 @@ class OLED:
 			return "R"
 
 	#----------------------------------
-	# Module: drawMap(self)
+	# Module: drawMap(self, ArrayofStrings)
 	# Description:
-	#
+	# 	Each pixel that is drawn corresponds to a character
+	# 	other than the SPACE character.  The ArrayofStrings
+	#	can have maximum index of MAX_PIXELS_ROW and each
+	#	string in the array can have maximum length of
+	#	MAX_PIXELS_COL.  Each Pixel is drawn one at a time.
 	#----------------------------------
-	def drawMap(self):
-		self.clear()
-		# Draw outline
-		self.oled.drawRoundedRectangle(0, 0, self.MAX_PIXELS_COL, self.MAX_PIXELS_ROW, 4, 1)
-                self.oled.setCursor(3,3)
-                self.oled.write("EIV")
-		# Draw map
-		grid = [" " for y in range(self.MAX_PIXELS_ROW)]
-		grid[0] = "                           |                                    "
-		grid[1] = "                           |                                    "
-		grid[2] = "                           |                                    "
-		grid[3] = "                           |                                    "
-		grid[4] = "                           |                                    "
-		grid[5] = "                           |     __________________________     "
-		grid[6] = "                           |     |                        |     "
-		grid[7] = "                           |     |                        |     "
-		grid[8] = "                           |     |                        |     "
-		grid[9] = "                           |     |                        |     "
-		grid[10]= "                           |     |                        |     "
-		grid[11]= "                           |     |                        |     "
-		grid[12]= "                           |     |                        |     "
-		grid[13] ="----------------------------     |                        |     "
-		grid[14] ="                                 |                        |     "
-		grid[15] ="                                 |                        |     "
-		grid[16] ="                                 |                        |     "
-		grid[17] ="                                 |                        |     "
-		grid[18] ="                                 |                        |     "
-		grid[19] ="     ____________________________|                        |     "
-		grid[20] ="     |                                                    |     "
-		for i in range(21, 42):
-			grid[i] = grid[20]
-		grid[42] ="     |                                                    |     "
-		grid[43] ="     ------------------------------------------------------     "
+	def drawMap(self, ArrayOfStrings):
 		x = 0
 		y = 0
-		for i in grid:
+		# For each string in the array
+		for i in ArrayOfStrings:
 			x = 0
+			# For each character in the string
 			for j in i:
+				# Any character other than space 
+				# corresponds to a pixel drawn on the map
 				if j != " ":
 					self.oled.drawPixel(x, y, 1)
 					self.oled.refresh()
 				x += 1
 			y += 1
-        
-	def updateMap(self, position):
-		# need to implement
-		self.oled.drawCircle(61, 45, 2, 1)
+
+	#----------------------------------
+	# Module: drawEIVMap(self)
+	# Description:
+	#	Clears the screen before drawing the map
+	#	Draws a map of UCLA EIV 4th Floor
+	# 	with map position indices
+	#----------------------------------
+	def drawEIVMap(self):
+		self.clear()
+		# Draw outline
+		self.oled.drawRoundedRectangle(0, 0, self.MAX_PIXELS_COL, self.MAX_PIXELS_ROW, 4, 1)
+		self.oled.setCursor(3,3)
+		self.oled.write("EIV")
+		
+		# Initialize MAP arrays of strings
+		grid = [" " for y in range(self.MAX_PIXELS_ROW)]
+		grid[0] = "                      |                                         "
+		for i in range(1,9):
+			grid[i] = grid[0]
+		grid[9] = "                      |                                         "
+		grid[10]= "                      |          _____________________          "
+		grid[11]= "                      |          |                   |          "
+		grid[12]= "                      |          |                   |          "
+		grid[13]= "                      |          |                   |          "
+		grid[14]= "-----------------------          |                   |          "
+		grid[15]= "                                 |                   |          "
+		for i in range(16, 25):
+			grid[i] = grid[15]
+		grid[25] ="          _______________________|                   |          "
+		grid[26] ="          |                                          |          "
+		for i in range(27, 37):
+			grid[i] = grid[26]
+		grid[37] ="          |                                          |          "
+		grid[38] ="          --------------------------------------------          "
+
+		# Draw EIV Map
+		self.drawMap(grid)
+		
+		# Draw Map Position References
+		self.oled.setCursor(39, 56)
+		self.oled.write("0")
+		self.oled.setCursor(39, 2)
+		self.oled.write("17")
+		self.oled.setCursor(16, 2)
+		self.oled.write("25")
+		self.oled.setCursor(16, 22)
+		self.oled.write("35")
+		self.oled.setCursor(2, 24)
+		self.oled.write("42")
+		self.oled.setCursor(2, 48)
+		self.oled.write("47")
 		self.oled.refresh()
+
+	#----------------------------------
+	# Position to pixel (row and column) mapping
+	#----------------------------------
+	POS = [{'ROW': 43, 'COL': 58}, {'ROW': 43, 'COL': 53}, {'ROW': 43, 'COL': 50}, {'ROW': 43, 'COL': 47}, {'ROW': 43, 'COL': 44},
+	{'ROW': 43, 'COL': 41}, {'ROW': 43, 'COL': 38}, {'ROW': 43, 'COL': 35}, {'ROW': 43, 'COL': 32}, {'ROW': 43, 'COL': 29},
+	{'ROW': 43, 'COL': 26}, {'ROW': 43, 'COL': 22}, {'ROW': 43, 'COL': 19}, {'ROW': 43, 'COL': 16}, {'ROW': 43, 'COL': 13},
+	{'ROW': 43, 'COL': 10}, {'ROW': 43, 'COL': 7}, {'ROW': 43, 'COL': 5}, {'ROW': 41, 'COL': 5}, {'ROW': 38, 'COL': 5},
+	{'ROW': 35, 'COL': 5}, {'ROW': 32, 'COL': 5}, {'ROW': 29, 'COL': 5}, {'ROW': 26, 'COL': 5}, {'ROW': 23, 'COL': 5},
+	{'ROW': 20, 'COL': 5}, {'ROW': 20, 'COL': 7}, {'ROW': 20, 'COL': 10}, {'ROW': 20, 'COL': 12}, {'ROW': 20, 'COL': 15},
+	{'ROW': 20, 'COL': 17}, {'ROW': 20, 'COL': 20}, {'ROW': 20, 'COL': 22}, {'ROW': 20, 'COL': 24}, {'ROW': 20, 'COL': 26},
+	{'ROW': 20, 'COL': 28}, {'ROW': 18, 'COL': 28}, {'ROW': 16, 'COL': 28}, {'ROW': 14, 'COL': 28}, {'ROW': 12, 'COL': 28},
+	{'ROW': 10, 'COL': 28}, {'ROW': 8, 'COL': 28}, {'ROW': 5, 'COL': 28}, {'ROW': 5, 'COL': 34}, {'ROW': 5, 'COL': 40},
+	{'ROW': 5, 'COL': 46}, {'ROW': 5, 'COL': 52}, {'ROW': 5, 'COL': 58}, {'ROW': 7, 'COL': 58}, {'ROW': 10, 'COL': 58},
+	{'ROW': 12, 'COL': 58}, {'ROW': 15, 'COL': 58}, {'ROW': 17, 'COL': 58}, {'ROW': 19, 'COL': 58}, {'ROW': 21, 'COL': 58},
+	{'ROW': 23, 'COL': 58}, {'ROW': 26, 'COL': 58}, {'ROW': 29, 'COL': 58}, {'ROW': 31, 'COL': 58}, {'ROW': 34, 'COL': 58},
+	{'ROW': 37, 'COL': 58}, {'ROW': 40, 'COL': 58}]
+	# Current Player Position in the map
+	PLAYER_POS = 0
+
+	#----------------------------------
+	# Module: updateMap(self, position)
+	# Description:
+	# 	drawEIVMap(self) should be called before this module.
+	# 	This module updates the location of the player without
+	#	having to redraw the map.  The module takes a parameter
+	# 	named position that takes values in the range (0, 61)
+	#----------------------------------
+	def updateMap(self, position):
+		# Erase Old Location
+		self.oled.drawCircleFilled(self.POS[self.PLAYER_POS]['COL'], self.POS[self.PLAYER_POS]['ROW'], 2, 0)
+		# Draw New Location
+		self.PLAYER_POS = position
+		self.oled.drawCircleFilled(self.POS[self.PLAYER_POS]['COL'], self.POS[self.PLAYER_POS]['ROW'], 2, 1)
+		# ReDraw Map Position References
+		self.oled.setCursor(39, 56)
+		self.oled.write("0")
+		self.oled.setCursor(39, 2)
+		self.oled.write("17")
+		self.oled.setCursor(16, 2)
+		self.oled.write("25")
+		self.oled.setCursor(16, 22)
+		self.oled.write("35")
+		self.oled.setCursor(2, 24)
+		self.oled.write("42")
+		self.oled.setCursor(2, 48)
+		self.oled.write("47")
+		self.oled.refresh()
+
