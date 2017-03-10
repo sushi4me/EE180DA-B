e1031bfd7a3de93cd1c7f16d288d3432a5b88afb
Fernando Pacheco <fpacheco0707@yahoo.com>
2017-03-10 00:06:51 +0000

    Adds check for needed directory

--- location/sample.sh
+++ location/sample.sh
@@ -11,6 +11,10 @@ COUNTER=0
 
 DIR="/home/root/EE180DA-B/location/observed_rssi/"
 
+if [ ! -d $DIR ]; then
+	mkdir $DIR
+fi	
+
 while [ $COUNTER -lt $NUMSAMPLES ]; do 
 	FILENAME="${DIR}sample${COUNTER}.txt"
 	iwlist wlan0 scan | grep 'Address:\|Signal' | sed 's/.*Address: //; s/.*\([0-9]\{2\}\) dBm/\1/' | sed 'N; s/\n/ /' > $FILENAME
